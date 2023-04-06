085619d548a8547682f73384fd3b25407c2dda79
Jakob Joachim <jakob.joachim@wps.de>
2023-04-06 08:19:57 +0200

    spreading

---
+++ spreading/README.md
@@ -0,0 +1,1 @@
+https://codesandbox.io/s/spreading-1nzp7g
---
+++ spreading/react/src/app.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import BetterButton from "./better-button";
+
+export default function App() {
+    return <BetterButton message="Hallo ihr lieben">Moin</BetterButton>;
+}
