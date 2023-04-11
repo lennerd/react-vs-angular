303c053746f54ef92de21f98fbb48fa2a53a1aa0
Jakob Joachim <jakob.joachim@wps.de>
2023-04-11 16:59:04 +0200

    fantastic

--- spreading/angular/src/app/app.module.ts
+++ spreading/angular/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
+import { BetterButtonComponent } from './better-button/better-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BetterButtonComponent
   ],
   imports: [
     BrowserModule,
