dfbe5ed26c17344373530feed8f4ab5453b60265
Lennart Hildebrandt <lennart.hildebrandt@wps.de>
2023-04-11 11:43:36 +0200

    Add todos

--- src/react-vs-angular.tsx
+++ src/react-vs-angular.tsx
@@ -8,7 +8,31 @@ export default function ReactVsAngular() {
         <Heading>React vs. Angular</Heading>
       </Slide>
       <Slide>
+        <Heading>Intro</Heading>
+      </Slide>
+
+      <Slide>
+        <Heading>React- vs. Angular-Komponenten</Heading>
+      </Slide>
+      <Slide>
         <CodeSandbox id="spreading-1nzp7g" />
+      </Slide>
+      <Slide>TODO Beispiel Props vs. @Input() (Typsicherheit)</Slide>
+      <Slide>TODO Beispiel: TSX vs. Compiler (Typsicherheit)</Slide>
+      <Slide>TODO Beispiel: Enum im Template verwenden</Slide>
+      <Slide>TODO Beispiel: Controlled vs. Uncontrolled</Slide>
+      <Slide>
+        TODO Beispiel: Value-UI? Form-Control-Beispiel mit React.Nodes?
+      </Slide>
+
+      <Slide>
+        <Heading>React-Hook vs. Angular-Service</Heading>
+      </Slide>
+      <Slide>TODO Beispiel, TODO Entwicklungsprozess</Slide>
+      <Slide>TODO Beispiel: Change-Detection: setTimeout?</Slide>
+
+      <Slide>
+        <Heading>Fazit</Heading>
       </Slide>
     </Deck>
   );
