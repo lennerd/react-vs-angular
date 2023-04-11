9d2c518cbc3f1c3448a0dedad88b6b722ec2bb3d
Lennart Hildebrandt <lennart.hildebrandt@wps.de>
2023-04-11 10:45:26 +0200

    Add spectacle

---
+++ src/components/code-sandbox.tsx
@@ -0,0 +1,73 @@
+import { memo, useContext, useEffect, useRef, useState } from "react";
+import { SlideContext } from "spectacle";
+import styled from "styled-components";
+
+const CodeSandboxIframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  border: 0;
+  border-radius: 0.5rem;
+  overflow: hidden;
+`;
+
+export interface CodeSandboxProps {
+  id: string;
+  module?: string;
+  view?: "editor" | "split" | "preview";
+  runOnClick?: boolean;
+}
+
+const CodeSandbox = memo(
+  ({ id, module, view, runOnClick = true }: CodeSandboxProps) => {
+    const { isSlideActive } = useContext(SlideContext);
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+    const [renderIframe, setRenderIframe] = useState(isSlideActive);
+
+    useEffect(() => {
+      if (!isSlideActive) {
+        return;
+      }
+
+      const timeout = setTimeout(() => {
+        setRenderIframe(true);
+      }, 50);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    }, [isSlideActive]);
+
+    const additionQueryParams: string[] = [];
+
+    if (module != null) {
+      additionQueryParams.push(`module=${module}`);
+    }
+
+    if (view != null) {
+      additionQueryParams.push(`view=${view}`);
+    }
+
+    additionQueryParams.push(`runonclick=${Number(runOnClick)}`);
+
+    let src = `https://codesandbox.io/embed/${id}?autoresize=1&fontsize=14&theme=dark`;
+
+    if (additionQueryParams.length > 0) {
+      src += `&${additionQueryParams.join("&")}`;
+    }
+
+    return (
+      <>
+        {renderIframe && (
+          <CodeSandboxIframe
+            ref={iframeRef}
+            src={src}
+            allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+          />
+        )}
+      </>
+    );
+  }
+);
+
+export default CodeSandbox;
--- src/react-vs-angular.tsx
+++ src/react-vs-angular.tsx
@@ -1,3 +1,15 @@
+import { Deck, Slide, Heading } from "spectacle";
+import CodeSandbox from "./components/code-sandbox.tsx";
+
 export default function ReactVsAngular() {
-  return null;
+  return (
+    <Deck>
+      <Slide>
+        <Heading>React vs. Angular</Heading>
+      </Slide>
+      <Slide>
+        <CodeSandbox id="spreading-1nzp7g" />
+      </Slide>
+    </Deck>
+  );
 }
