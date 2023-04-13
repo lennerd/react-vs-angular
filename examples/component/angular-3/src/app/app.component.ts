6c63cb11357b549bbb82f36b8cd2152cf1678e4a
Jakob Joachim <jakob.joachim@wps.de>
2023-04-13 14:36:51 +0200

    example ui component

---
+++ examples/component/angular-3/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  protected isDisabled = Math.random() > 0.5;
+
+  protected click(event: Event) {
+    if (event.defaultPrevented) {
+      return;
+    }
+    console.log("You clicked me!");
+  }
+}
