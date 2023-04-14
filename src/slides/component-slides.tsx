c0ed0904a26178843130d327e87d6b0c22021f8d
Jakob Joachim <jakob.joachim@wps.de>
2023-04-14 08:43:19 +0200

    jetzte?

--- src/slides/component-slides.tsx
+++ src/slides/component-slides.tsx
@@ -10,7 +10,7 @@ export default function ComponentSlides() {
       </SlideLayout.Center>
       <Slide>
         <StackBlitz
-          id="github/lennerd/react-vs-angular/tree/main/examples/component/reacting"
+          id="github/lennerd/react-vs-angular/tree/main/examples/component/react-0"
           file="src/app.tsx"
         />
       </Slide>
